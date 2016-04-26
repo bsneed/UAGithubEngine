1aa515022b0454bc2d5fb48e372ad43cf29b27f3
Brandon Sneed <brandon@redf.net>
2016-04-26 09:21:49 -0700

    Added static lib target, and fixed compiler warnings.

---
+++ UAGithubEngine_static/UAGithubEngine_static.h
@@ -0,0 +1,13 @@
+//
+//  UAGithubEngine_static.h
+//  UAGithubEngine_static
+//
+//  Created by Brandon Sneed on 4/26/16.
+//  Copyright © 2016 Walmart. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UAGithubEngine_static : NSObject
+
+@end
---
+++ UAGithubEngine_static/UAGithubEngine_static.m
@@ -0,0 +1,13 @@
+//
+//  UAGithubEngine_static.m
+//  UAGithubEngine_static
+//
+//  Created by Brandon Sneed on 4/26/16.
+//  Copyright © 2016 Walmart. All rights reserved.
+//
+
+#import "UAGithubEngine_static.h"
+
+@implementation UAGithubEngine_static
+
+@end
